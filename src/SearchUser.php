d6c1765061ef7b643d54956245c3a47b9dc03483
Jayden Bailey <jaydenkieran@gmail.com>
2025-01-02 03:33:12 +0000

    Allow looking up user by email or ID

--- src/SearchUser.php
+++ src/SearchUser.php
@@ -10,10 +10,13 @@ use MediaWiki\Html\Html;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 use MediaWiki\User\UserFactory;
+use Wikimedia\Rdbms\ILoadBalancer;
 
 class SearchUser extends GloopControlSubpage {
 
 	private UserFactory $userFactory;
+
+	private ILoadBalancer $loadBalancer;
 
 	private ExtensionRegistry $er;
 
@@ -21,6 +24,7 @@ class SearchUser extends GloopControlSubpage {
 
 	function __construct( SpecialGloopControl $special ) {
 		$this->userFactory = MediaWikiServices::getInstance()->getUserFactory();
+		$this->loadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancerFactory()->getMainLB();
 		$this->er = ExtensionRegistry::getInstance();
 		$this->lang = $special->getLanguage();
 		parent::__construct( $special );
@@ -34,12 +38,35 @@ class SearchUser extends GloopControlSubpage {
 	private function displayForm() {
 		// Build the form
 		$desc = [
+			'type' => [
+				'type' => 'select',
+				'required' => true,
+				'label-message' => 'gloopcontrol-user-type',
+				'options' => [
+					'Username' => 'username',
+					'ID' => 'id',
+					'Email address' => 'email',
+				]
+			],
 			'user' => [
 				'type' => 'user',
 				'cssclass' => 'mw-autocomplete-user',
 				'label-message' => 'gloopcontrol-user-username',
 				'exists' => true,
-				'required' => true
+				'required' => true,
+				'hide-if' => [ '!==', 'type', 'username' ]
+			],
+			'id' => [
+				'type' => 'int',
+				'label-message' => 'gloopcontrol-user-id',
+				'required' => true,
+				'hide-if' => [ '!==', 'type', 'id' ]
+			],
+			'email' => [
+				'type' => 'email',
+				'label-message' => 'gloopcontrol-user-email',
+				'required' => true,
+				'hide-if' => [ '!==', 'type', 'email' ]
 			]
 		];
 
@@ -52,16 +79,62 @@ class SearchUser extends GloopControlSubpage {
 
 	public function onFormSubmit( $formData ) {
 		$out = $this->special->getOutput();
-		$name = $formData[ 'user' ];
+		$type = $formData[ 'type' ];
 
-		// Lookup the user's info
-		$user = $this->userFactory->newFromName( $name );
+		if ( $type === 'username' ) {
+			$name = $formData[ 'user' ];
+			$user = $this->userFactory->newFromName( $name );
+		} else if ( $type === 'id' ) {
+			$id = $formData[ 'id' ];
+			$user = $this->userFactory->newFromId( $id );
+			$user->loadFromId();
+		} else if ( $type === 'email' ) {
+			$email = $formData[ 'email' ];
+
+			// Find user in the DB by the email address
+			$res = $this->loadBalancer->getConnection( DB_PRIMARY )->newSelectQueryBuilder()
+				->select( '*' )
+				->from( 'user' )
+				->where( [ 'user_email' => $email ] )
+				->caller( __METHOD__ )
+				->fetchResultSet();
+
+			$numRows = $res->numRows();
+			if ( $numRows === 0 ) {
+				$out->addHTML(Html::errorBox(
+					$out->msg( 'gloopcontrol-user-not-found', $email )
+						->parse()
+				));
+				return;
+			} else if ( $numRows === 1 ) {
+				// There is one user with this email address
+				$row = $res->current();
+			} else {
+				// There are multiple users with this email address
+				$html = [];
+				foreach ( $res as $row ) {
+					$html[] = $row->user_name;
+				}
+
+				$out->addHTML(Html::noticeBox(
+					$out->msg( 'gloopcontrol-user-multiple-email', $email )
+						->parse() . '<ul><li>' . implode( '</li><li>', $html ) . '</li></ul>',
+					'gloopcontrol-user-multiple-email'
+				));
+				return;
+			}
+
+			$user = $this->userFactory->newFromRow( $row );
+		} else {
+			return;
+		}
+
 		if ( $user === null || $user->getId() === 0 ) {
-			// The form should already validate if a user exists or not - this is just here for redundancy.
 			$out->addHTML(Html::errorBox(
-				$out->msg( 'gloopcontrol-user-not-found', $name )
+				$out->msg( 'gloopcontrol-user-not-found', $name ?? $email ?? $id ?? '' )
 					->parse()
 			));
+			return;
 		}
 
 		$emailAuth = $user->getEmailAuthenticationTimestamp();
