b4b55b604435bf2a4e477f4dcf0572d6ab01c7e4
Jayden Bailey <jaydenkieran@gmail.com>
2026-03-09 21:19:20 +0000

    anonymisation: remove PII from CU and some logs, use RenameUserFactory correctly

--- src/tasks/AnonymiseUserTask.php
+++ src/tasks/AnonymiseUserTask.php
@@ -4,12 +4,15 @@ namespace MediaWiki\Extension\GloopControl\Tasks;
 
 use MediaWiki\Auth\AuthManager;
 use MediaWiki\MediaWikiServices;
-use MediaWiki\RenameUser\RenameuserSQL;
+use MediaWiki\RenameUser\RenameUserFactory;
 use MediaWiki\Session\SessionManager;
 use MediaWiki\Status\Status;
 use MediaWiki\User\User;
 use MediaWiki\User\UserFactory;
 use StatusValue;
+use Wikimedia\Rdbms\DBConnRef;
+use Wikimedia\Rdbms\DBQueryError;
+use Wikimedia\Rdbms\IDatabase;
 
 /**
  * Task to anonymize a user.
@@ -29,11 +32,19 @@ class AnonymiseUserTask {
 	/** @var SessionManager */
 	private SessionManager $sessionManager;
 
+	/** @var RenameUserFactory */
+	private RenameUserFactory $renameUserFactory;
+
+	/** @var DBConnRef */
+	private DBConnRef $dbw;
+
 	public function __construct() {
 		$services = MediaWikiServices::getInstance();
 		$this->userFactory = $services->getUserFactory();
 		$this->authManager = $services->getAuthManager();
 		$this->sessionManager = $services->getSessionManager();
+		$this->dbw = $services->getDBLoadBalancer()->getMaintenanceConnectionRef( DB_PRIMARY );
+		$this->renameUserFactory = $services->getRenameUserFactory();
 	}
 
 	/**
@@ -48,24 +59,71 @@ class AnonymiseUserTask {
 			return $status->fatal( 'gloopcontrol-tasks-error-user-temp' );
 		}
 
+		// Change various settings for the user, and invalidate their sessions
 		$user->setRealName( '' );
-
 		$this->authManager->revokeAccessForUser( $user->getName() );
 		$user->invalidateEmail();
 		$user->setToken( User::INVALID_TOKEN );
 		$user->saveSettings();
 		$this->sessionManager->preventSessionsForUser( $user->getName() );
 
-		$rename = new RenameuserSQL(
-			$user->getName(),
+		// Rename the user
+		$rename = $this->renameUserFactory->newRenameUser(
+			$performingUser,
+			$user,
 			$this->userFactory->newFromName( 'Anonymous ' .
 				MediaWikiServices::getInstance()->getGlobalIdGenerator()->newUUIDv4() )->getName(),
-			$user->getId(),
-			$performingUser,
-			[ 'reason' => 'Anonymizing' ]
+			'Anonymizing'
 		);
-		if ( !$rename->renameUser() ) {
+		if ( !$rename->renameUnsafe() ) {
 			return $status->fatal( 'gloopcontrol-tasks-error-user-anonymize-failed', $user->getName() );
+		}
+
+		// Delete any potential PII in the database
+		$actorId = $user->getActorId();
+		$rowsToDelete = [
+			'logging' => [
+				// Delete any logs related to the rename
+				[ 'log_action' => 'renameuser', 'log_title' => $user->getTitleKey(), 'log_type' => 'renameuser' ]
+			],
+			'recentchanges' => [
+				// Delete any logs related to the rename
+				[ 'rc_log_action' => 'renameuser', 'rc_title' => $user->getTitleKey(), 'rc_log_type' => 'renameuser' ]
+			],
+			// CheckUser PII
+			'cu_changes' => [
+				[ 'cuc_actor' => $actorId ]
+			],
+			'cu_log' => [
+				[ 'cul_actor' => $actorId ],
+				[ 'cul_target_id' => $actorId, 'cul_type' => [ 'useredits', 'userips' ] ]
+			]
+		];
+
+		$dbw = $this->dbw;
+		foreach ( $rowsToDelete as $key => $value ) {
+			if ( !$dbw->tableExists( $key, __METHOD__ ) ) {
+				continue;
+			}
+
+			foreach ( $value as $where ) {
+				try {
+					$method = __METHOD__;
+					$dbw->doAtomicSection( $method, static function () use ( $dbw, $key, $where, $method ) {
+						$dbw->newDeleteQueryBuilder()
+							->deleteFrom( $key )
+							->where( $where )
+							->caller( $method )
+							->execute();
+					}, IDatabase::ATOMIC_CANCELABLE );
+				} catch ( DBQueryError $e ) {
+					$status->warning(
+						'gloopcontrol-tasks-error-user-anonymize-failed-dbdelete',
+						$user->getName(),
+						$e->getMessage()
+					);
+				}
+			}
 		}
 
 		return $status::newGood( wfMessage( 'gloopcontrol-tasks-success-anonymize', $user->getName() ) );
